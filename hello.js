692049572c16dfe79618ab7206057aeaa63ec0d7
mukakanyenzi <ernestine.mukakanyenzi@capgemini.com>
2019-03-11 14:00:53 +0100

    ajout d'un fichier hello

---
+++ hello.js
@@ -0,0 +1,9 @@
+var surnom ="Tina";
+var y_ nom = "mukakanyenzi";
+var pren = "tina";
+  function cmtTPT(){
+	return nom +" "+pren;
+}
+alert("bonjour "+nom+" "+pren);
+var massageQ = "comment vas- tu?";
+alert(messageQ);
